d75b60118e724ec2725f3bf6374f216cbe98e866
zenin5479 <zenin5479@gmail.com>
2025-09-12 20:05:07 +0300

    Update 0.4 Обновлен метод. Проведен рефакторинг

--- VariousMethods.cs
+++ VariousMethods.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace LibraryForStruct
 {
-   public class MethodsForStruct
+   public class VariousMethods
    {
       // Определяем структуру
       public struct Student
